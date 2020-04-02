8fe5d49c45df8b8d429f6e1d14e048865db14e59
Ashley Claiborne <languageisaliquid@gmail.com>
2020-04-01 21:15:24 -0500

    Add project files.

---
+++ MyFirstWebApi/Controllers/AnimalsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApi.Models;
+
+namespace MyFirstWebApi.Controllers
+{
+    [Route("api/animals")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+       static List<Animals> _animals = new List<Animals>
+                                    { 
+                                        new Animals { Id = 1, Name="Mark", Type= "Elephant", Weight= 1000 },
+                                        new Animals { Id = 2, Name="Derek", Type= "Zebra", Weight= 500 },
+                                        new Animals { Id = 3, Name="Murphy", Type= "Dog", Weight= 50 }
+                                    };
+
+        [HttpGet]
+        public IActionResult GetAllAnimals()
+        {
+            return Ok(_animals);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleAnimal(int id) 
+        {
+            var selectedAnimal = _animals.FirstOrDefault(a => a.Id == id);
+
+            if (selectedAnimal ==  null)
+            {
+                return NotFound($"Could not find an animal matching the id {id}");
+            }
+            return Ok(selectedAnimal);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAnimal(Animals animalToAdd)
+        {
+            _animals.Add(animalToAdd);
+            return Ok(_animals);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAnimal(int id, Animals animal)
+        {
+            var animalToChange = _animals.FirstOrDefault(a => a.Id == id);
+            if (animalToChange == null)
+            {
+                return NotFound("Cannot update; this animal does not exist");
+            }
+
+            animalToChange.Name = animal.Name;
+            animalToChange.Weight = animal.Weight;
+            animalToChange.Type = animal.Type;
+
+            return Ok(animalToChange);
+        }
+    }
+}
---
+++ MyFirstWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyFirstWebApi.Controllers
+{
+    [Route("api/values")] //name should be api/[name before "Controller" in the class], so ValuesController, api/values.  This is the url/end point that the controller should be available from 
+    [ApiController] // what kind of controller to make available
+    public class ValuesController : ControllerBase 
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> ObtainValueStrings()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/{id}
+        [HttpGet("{id}")] // can do multiple with "/" between: [HttpGet("{id}/{name}/{price}")] leave out of curly braces to hard code text(literal)
+        public IActionResult EchoBackANumber(int id)
+        {
+            if (id == 1000)
+            {
+                return NotFound("You suck at life.");
+            }
+
+            return Ok(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
